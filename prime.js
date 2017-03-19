dd88343ba2d84db98a9942f43ff477de39b84e49
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-19 12:51:17 -0700

    Add primeFactors function

--- prime.js
+++ prime.js
@@ -1,15 +1,35 @@
+// Write a function to verify if a number is prime
 function isPrime (n) {
-  let divisor = 2;
+  let divisor = 2
   while (Math.sqrt(n) >= divisor) {
-    if (n % divisor == 0) {
-      return false;
+    if (n % divisor === 0) {
+      return false
     }
     else {
-      divisor++;
+      divisor++
     }
   }
   return true
-};
+}
 
-console.log(isPrime(137));
-console.log(isPrime(237));
+console.log(isPrime(137))
+console.log(isPrime(237))
+
+// Write a function to find all prime factors of a number
+function primeFactors (n) {
+  const factors = []
+  let divisor = 2
+
+  while (n > 2) {
+    if (n % divisor === 0) {
+      factors.push(divisor)
+      n = n / divisor
+    }
+    else {
+      divisor++
+    }
+  }
+  return factors
+}
+
+console.log(primeFactors(69))
