e741f53f8a1674751a16446cc2474b18be8d7711
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-19 17:28:16 -0700

    Create duplicates.js

---
+++ duplicates.js
@@ -0,0 +1,15 @@
+// Write a function to remove duplicates from an array
+function removeDuplicates (array) {
+  let exists = {}
+  let uniqueArr = []
+  for (var i = 0; i < array.length; i++) {
+    let el = array[i]
+    if (!exists[el]) {
+      uniqueArr.push(el)
+      exists[el] = true
+    }
+  }
+  return uniqueArr
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
