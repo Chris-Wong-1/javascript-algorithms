18656d66ad0bbcd71f6b5dde0d72d842b5210f8f
chris-wong-1 <wong.christopher.s@gmail.com>
2017-03-22 19:19:20 -0700

    Add fibonacci function with memoization

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,27 @@ function fibonacci (n) {
 }
 
 console.log(fibonacci(12))
+
+// Add basic memoization to the function to improve performance on larger numbers
+
+var memoFibonacci = ( function () {
+  const memo = {}
+
+  function fib (n) {
+    let value
+    if (n in memo) {
+      value = memo[n]
+    } else {
+      if (n <= 1) {
+        value = n
+      } else {
+        value = fib(n - 1) + fib(n - 2)
+      }
+      memo[n] = value
+    }
+    return value
+  }
+  return fib
+})()
+
+console.log(memoFibonacci(40))
