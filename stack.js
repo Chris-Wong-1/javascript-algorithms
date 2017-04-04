b04f78e061dce3f44f200238f5ed07ec07ffae70
chris-wong-1 <wong.christopher.s@gmail.com>
2017-04-04 15:04:21 -0700

    Add queue implementation

--- stack.js
+++ stack.js
@@ -1,14 +1,29 @@
 // Implementation of a last in first out stack
-function lifoStack() {
-  this.stack = new Array();
+function lifoStack () {
+  this.stack = new Array()
 
   // Add a new item to the top of the stack
-  this.push = function(item) {
-    return this.stack.push(item);
+  this.push = function (item) {
+    return this.stack.push(item)
   }
 
   // Remove the top item from the stack
-  this.pop = function() {
-    return this.stack.pop();
+  this.pop = function () {
+    return this.stack.pop()
+  }
+}
+
+// Implementation of a queue, also a first in first out stack
+function fifoQueue () {
+  this.queue = new Array()
+
+  // Add a new item to the queue
+  this.enqueue = function (item) {
+    this.queue.unshift(item)
+  }
+
+  // Remove the first item in the queue
+  this.dequeue = function () {
+    return this.queue.pop()
   }
 }
