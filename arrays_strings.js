453463417dc7e466ef632cbcd425914e8b5c0d24
chris-wong-1 <wong.christopher.s@gmail.com>
2017-04-05 15:00:58 -0700

    Add function to check string permutations

--- arrays_strings.js
+++ arrays_strings.js
@@ -1,60 +1,41 @@
 // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-function isUnique(string) {
-  let currentLetter = '';
-  for (var i = 0; i < string.length; i++) {
-    currentLetter = string[i];
-    for (var j = 0; j < string.length; j++) {
+function isUnique (string) {
+  let currentLetter = ''
+  for (let i = 0; i < string.length; i++) {
+    currentLetter = string[i]
+    for (let j = 0; j < string.length; j++) {
       if (currentLetter === string[j] && i != j) {
-        return false;
-      };
-    };
-  };
-  return true;
+        return false
+      }
+    }
+  }
+  return true
 }
 
-// console.log(isUnique("cat"));
-// console.log(isUnique("mom"));
+// console.log(isUnique("cat"))
+// console.log(isUnique("mom"))
 
-//
-// def unique_with_hash?(str)
-//   chars_hash = Hash.new(0)
-//   chars = str.split("")
-//   chars.each do |char|
-//     chars_hash[char] += 1
-//   end
-//   chars_hash.values.each do |value|
-//     if value > 1
-//       return false
-//     end
-//     return true
-//   end
-// end
-//
 // # 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
 
-function charCount(str) {
-  let chars = {};
-  for (var i = 0; i < str.length; i++) {
-    chars.str[i] += 1;
-  };
-  return chars
+function isPermutation (str1, str2) {
+  // Convert the strings into sorted arrays
+  let arr1 = str1.split('').sort()
+  let arr2 = str2.split('').sort()
+  // Check for edge cases
+  if (arr1 === arr2) return true
+  if (arr1 == null || arr2 == null) return false
+  if (arr1.length != arr2.length) return false
+  // Check that each element of the sorted arrays match
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] != arr2[i]) return false
+  }
+  return true
 }
 
-console.log(charCount("turtle"))
+// console.log(isPermutation('cat', 'tac'))
+// console.log(isPermutation('dog', 'cat'))
 
-function isPermutation(str1, str2) {
-
-}
-
-//
-// def permutation(str1, str2)
-//   str1.split('').sort == str2.split('').sort
-// end
-//
-// # p permutation('cat', 'tac')
-// # p permutation('cat', 'dog')
-//
 // # 1.3 URLify: Write a method to replace all spaces in a string with '%20'.
 //
 // def urlify(str)
